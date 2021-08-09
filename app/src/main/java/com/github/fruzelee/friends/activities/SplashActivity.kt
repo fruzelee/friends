cf503b981e82306c5034b2840abe15e3ccacaa80
Fazle Rabbi <fazlerabbicse@gmail.com>
2021-08-09 22:30:45 +0600

    Added the Animation to the splash screen and make it as launcher activity

--- app/src/main/java/com/github/fruzelee/friends/activities/SplashActivity.kt
+++ app/src/main/java/com/github/fruzelee/friends/activities/SplashActivity.kt
@@ -1,12 +1,56 @@
 package com.github.fruzelee.friends.activities
 
+import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.WindowInsets
+import android.view.WindowManager
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import com.github.fruzelee.friends.MainActivity
 import com.github.fruzelee.friends.R
+import com.github.fruzelee.friends.databinding.ActivitySplashBinding
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+
+        val splashBinding: ActivitySplashBinding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(splashBinding.root)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            @Suppress("DEPRECATION")
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+
+        val splashAnimation = AnimationUtils.loadAnimation(this@SplashActivity, R.anim.anim_splash)
+        splashBinding.tvAppName.animation = splashAnimation
+
+        splashAnimation.setAnimationListener(object : Animation.AnimationListener {
+            override fun onAnimationStart(animation: Animation?) {
+                // "execute when animation starts")
+            }
+
+            override fun onAnimationEnd(animation: Animation?) {
+                // "execute when animation ends")
+
+                Handler(Looper.getMainLooper()).postDelayed({
+                    startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+                    finish()
+                }, 1000)
+            }
+
+            override fun onAnimationRepeat(animation: Animation?) {
+                // "execute when animation repeats")
+            }
+        })
     }
 }
