592d62b33e661e8763f75c36a62e8bb3e8673f19
Fazle Rabbi <fazlerabbicse@gmail.com>
2021-08-09 21:53:19 +0600

    -new: added splash screen -update: color.xml

---
+++ app/src/main/java/com/github/fruzelee/friends/activities/SplashActivity.kt
@@ -0,0 +1,12 @@
+package com.github.fruzelee.friends.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.github.fruzelee.friends.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
